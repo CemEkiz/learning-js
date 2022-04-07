56aeebfb82d210daa80e5c1fd6e25c3ffbcbf7aa
Ekiz Cem <ekizcem.pro@gmail.com>
2022-04-07 13:57:32 +0200

    Variable Challenge and Control Flow ChAllenge

---
+++ codingchallenge.js
@@ -0,0 +1,33 @@
+//////////////////// Coding Challenge #1 ////////////////////
+
+// // // With Data 1
+// // const marksMass = 78;
+// // const marksHeight = 1.69;
+// // const johnMass = 92;
+// // const johnHeight = 1.95;
+
+// // With Data 2
+// const marksMass = 95;
+// const marksHeight = 1.88;
+// const johnMass = 85;
+// const johnHeight = 1.76;
+
+// const marksBMI = marksMass / marksHeight ** 2;
+// console.log(marksBMI);
+// const johnBMI = johnMass / johnHeight ** 2;
+// console.log(johnBMI);
+
+// const marksHigherBMI = marksBMI > johnBMI;
+// console.log(marksHigherBMI);
+
+// //////////////////// Coding Challenge #2 ////////////////////
+
+// if (marksBMI > johnBMI) {
+// 	console.log(
+// 		`Mark's BMI (${marksBMI}) is higher than John's BMI (${johnBMI})`
+// 	);
+// } else {
+// 	console.log(
+// 		`John's BMI (${johnBMI}) is higher than Marks's BMI (${marksBMI})`
+// 	);
+// }
