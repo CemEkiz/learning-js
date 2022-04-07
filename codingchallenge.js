3157815b292d3779c77218047976dd96a59acd72
Ekiz Cem <ekizcem.pro@gmail.com>
2022-04-07 17:08:06 +0200

    coding challenge (control flow with boolean logic)

--- codingchallenge.js
+++ codingchallenge.js
@@ -1,6 +1,7 @@
 //////////////////// Coding Challenge #1 ////////////////////
 
-// // // With Data 1
+// // 1)
+// // With Data 1
 // // const marksMass = 78;
 // // const marksHeight = 1.69;
 // // const johnMass = 92;
@@ -12,11 +13,13 @@
 // const johnMass = 85;
 // const johnHeight = 1.76;
 
+// // 2)
 // const marksBMI = marksMass / marksHeight ** 2;
 // console.log(marksBMI);
 // const johnBMI = johnMass / johnHeight ** 2;
 // console.log(johnBMI);
 
+// // 3)
 // const marksHigherBMI = marksBMI > johnBMI;
 // console.log(marksHigherBMI);
 
@@ -30,4 +33,60 @@
 // 	console.log(
 // 		`John's BMI (${johnBMI}) is higher than Marks's BMI (${marksBMI})`
 // 	);
+// }
+
+// //////////////////// Coding Challenge #3 ////////////////////
+
+// // 1)
+// const averageScoreDolphins = (96 + 108 + 89) / 3;
+// console.log(averageScoreDolphins);
+// const averageScoreKoalas = (88 + 91 + 110) / 3;
+// console.log(averageScoreKoalas);
+
+// // 2)
+// if (averageScoreDolphins > averageScoreKoalas) {
+// 	console.log("Dolphins win!");
+// } else if (averageScoreDolphins === averageScoreKoalas) {
+// 	console.log("There is a draw!");
+// } else {
+// 	console.log("Koalas win!");
+// }
+
+// // Bonus 1
+// const averageScoreDolphins = (97 + 112 + 101) / 3;
+// console.log(averageScoreDolphins);
+// const averageScoreKoalas = (109 + 95 + 123) / 3;
+// console.log(averageScoreKoalas);
+
+// if (averageScoreDolphins > averageScoreKoalas && averageScoreDolphins >= 100) {
+// 	console.log("Dolphins win!");
+// } else if (
+// 	averageScoreKoalas > averageScoreDolphins &&
+// 	averageScoreKoalas >= 100
+// ) {
+// 	console.log("Koalas win!");
+// } else {
+// 	console.log("No ones win!");
+// }
+
+// // Bonus 2
+// const averageScoreDolphins = (97 + 112 + 101) / 3;
+// console.log(averageScoreDolphins);
+// const averageScoreKoalas = (109 + 95 + 106) / 3;
+// console.log(averageScoreKoalas);
+
+// if (averageScoreDolphins > averageScoreKoalas && averageScoreDolphins >= 100) {
+// 	console.log("Dolphins win!");
+// } else if (
+// 	averageScoreKoalas > averageScoreDolphins &&
+// 	averageScoreKoalas >= 100
+// ) {
+// 	console.log("Koalas win!");
+// } else if (
+// 	(averageScoreDolphins === averageScoreKoalas && averageScoreDolphins,
+// 	averageScoreKoalas >= 100)
+// ) {
+// 	console.log("There is a draw!");
+// } else {
+// 	console.log("No ones win!");
 // }
