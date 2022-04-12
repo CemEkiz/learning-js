551a555794ca2cabb875901bc72fdefd9b8b803f
Ekiz Cem <ekizcem.pro@gmail.com>
2022-04-12 18:55:32 +0200

    Learning : values, variables, data types, let/const/var, operators, templates literals, if/else/switch statement, type conversion/coercion

---
+++ 01-JS-Fundamentals-1/assignments.js
@@ -0,0 +1,7 @@
+///// Assignments 1 : Values and Variables
+// let myCountry = "France";
+// let myContinent = "Europe";
+// let frenchPopulation = 67000000;
+// console.log(myCountry);
+// console.log(myContinent);
+// console.log(frenchPopulation);
