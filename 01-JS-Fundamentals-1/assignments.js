7ddf5c8c0fc0221b3bfb19064143159d0568e603
Ekiz Cem <ekizcem.pro@gmail.com>
2022-04-12 18:53:22 +0200

    remove all

--- 01-JS-Fundamentals-1/assignments.js
+++
@@ -1,7 +0,0 @@
-///// Assignments 1 : Values and Variables
-// let myCountry = "France";
-// let myContinent = "Europe";
-// let frenchPopulation = 67000000;
-// console.log(myCountry);
-// console.log(myContinent);
-// console.log(frenchPopulation);
