71b161b37a381195e16ac6ff2fda3b748d487ffe
Ekiz Cem <ekizcem.pro@gmail.com>
2022-06-08 18:02:56 +0200

    .bind Method with Functions

--- 10-Function/script.js
+++ 10-Function/script.js
@@ -217,15 +217,15 @@
 // const greetArrow1 = (greeting) => (name) => console.log(`${greeting} ${name}`);
 // greetArrow1("Hello")("Cem"); // Hello Cem
 
-// // /* Méthode 2 (pour information) */
-// // const greetArrow2 = (greeting) => {
-// // 	return function (name) {
-// // 		console.log(`${greeting} ${name}`);
-// // 	};
-// // };
+// /* Méthode 2 (pour information) */
+// const greetArrow2 = (greeting) => {
+// 	return function (name) {
+// 		console.log(`${greeting} ${name}`);
+// 	};
+// };
 
-// // const greetHello2 = greetArrow2("Hello");
-// // greetHello2("Cem"); // Hello Cem
+// const greetHello2 = greetArrow2("Hello");
+// greetHello2("Cem"); // Hello Cem
 
 // // ================================================================================ //
 // // ================================================================================ //
@@ -260,19 +260,19 @@
 // console.log(lufthansa); // cf. Console
 // console.log(lufthansa.bookings); // cf. Console
 
-// /* Je stocke dans la Method book dans une variable, la fonction devient alors une Regular Fonction,
+// /* Je stocke la Method .book dans une variable, la fonction devient alors une Regular Fonction,
 // ce n'est pas une Method qui appartient à l'Object lufthansa mais c'est une copie séparée */
 // const book = lufthansa.book;
 
-// /* -------------------------------- Call Method -------------------------------- */
+// /* -------------------------------- .call Method -------------------------------- */
 
-// /* ___Sans Call Method___ */
+// /* ___Sans .call Method___ */
 // /* La fonctionne renvoie une erreur à cause du "this" keyword, il faut alors préciser à quel Object
 // le "this" correspond explicitement (avec les Methods des fonctions) */
 // // book(23, "Sarah Williams"); // TypeError
 
-// /* ___Avec Call Method___ */
-// /* Call Method : le premier argument désigne la cible du this keyword */
+// /* ___Avec .call Method___ */
+// /* .call Method : le premier argument désigne la cible du this keyword */
 // book.call(eurowings, 23, "Sarah Williams"); // Sarah Williams booked a seat on Eurowings flight EW23
 // console.log(eurowings); // cf. Console
 // console.log(eurowings.bookings); // cf. Console
@@ -281,9 +281,9 @@
 // console.log(lufthansa); // cf. Console
 // console.log(lufthansa.bookings); // cf. Console
 
-// /* -------------------------------- Apply Method -------------------------------- */
-// /* C'est une Method similaire à Call Method mais il faut créer un Array d'abord qui sera le second argument
-// tandis que le premier restera (comme pour Call) la cible à designer pour le this keyword */
+// /* -------------------------------- .apply Method -------------------------------- */
+// /* C'est une Method similaire à .call Method mais il faut créer un Array d'abord qui sera le second argument
+// tandis que le premier restera (comme pour .call) la cible à designer pour le this keyword */
 
 // const flightData = [583, "George Cooper"];
 // book.apply(eurowings, flightData); // George Cooper booked a seat on Eurowings flight EW583
@@ -292,3 +292,110 @@
 
 // /* Mais il est conseillé d'utiliser call tout de même qui est la nouvelle Method en combinaison avec le Spread Operator */
 // book.call(eurowings, ...flightData); // George Cooper booked a seat on Eurowings flight EW583
+
+// ================================================================== //
+// ================================================================== //
+// ========================== .bind Method ========================== //
+// ================================================================== //
+// ================================================================== //
+
+// const lufthansa = {
+// 	airline: "Lufthansa",
+// 	iataCode: "LH",
+// 	bookings: [],
+// 	book(flightNum, name) {
+// 		console.log(
+// 			`${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+// 		);
+// 		this.bookings.push({
+// 			flight: `${this.iataCode}${flightNum}`,
+// 			name,
+// 		});
+// 	},
+// };
+
+// const eurowings = {
+// 	airline: "Eurowings",
+// 	iataCode: "EW",
+// 	bookings: [],
+// };
+
+// /* -------------------------------- .bind Method -------------------------------- */
+
+// /* La Method .bind fonctionne différement : elle crée une nouvelle fonction (SANS appeler la fonction) qui redirige le this keyword
+// vers le nouvel Object souhaité */
+
+// /* 1 - Il faut d'abord stocker la Method dans une variable afin de la convertir en Regular Function (comme pour .call et .apply) */
+// const book = lufthansa.book;
+
+// /* 2 - Il faut ensuite stocker la fonction dans une autre variable en utilisant la Method .bind */
+// const bookEW = book.bind(eurowings);
+// const bookLUF = book.bind(lufthansa);
+
+// /* 3 - Enfin, on peut utiliser la nouvelle fonction de manière classique */
+// bookEW(23, "Steven Williams"); // Steven Williams booked a seat on Eurowings flight EW23
+// bookLUF(45, "James Roger"); // James Roger booked a seat on Lufthansa flight LH45
+// console.log(eurowings); // cf. Console
+// console.log(lufthansa); // cf. Console
+
+// /* -------------------------------- Partial Application -------------------------------- */
+
+// /* _____Exemple 1_____ */
+// /* Il est aussi possible de définir un argument en avance lors de la création de la fonction : on appelle ça "Partial Application"
+// cela signifie que une partie des arguments de la fonction est déjà définie */
+// const bookEW55 = book.bind(eurowings, 55);
+// bookEW55("Thomas Cooper"); // Thomas Cooper booked a seat on Eurowings flight EW55
+
+// /* _____Exemple 2_____ */
+
+// const addTax = (rate, value) => value + value * rate;
+
+// /* Sans Partial Application : utilisation classique d'une fonction, il faut définir les 2 arguments */
+// console.log(addTax(0.2, 100)); // 120
+
+// /* Avec Partial Application : il faut bind les arguments "par défaut", null est obligatoire car c'est l'emplacement de .this et il n'est
+// pas présent dans cette fonction, puis la seconde valeur correspond à rate, etc. (L'ordre des arguments est donc très important !)
+// Au moment du call de la fonction il suffit de définir l'argument pour value */
+
+// const addVAT = addTax.bind(null, 0.2);
+// console.log(addVAT(100)); // 120
+
+// /* Pourquoi ne pas utiliser Default Parameters ?
+// Car avec la Method .bind  cela crée une nouvelle fonction plus spécifique et qui est séparée de la fonction d'origine (qui sert de base) */
+
+// /* Alternative : Function returning Function */
+// const addTax2 = function (rate) {
+// 	return function (price) {
+// 		console.log(price + price * rate);
+// 	};
+// };
+
+// const addVAT2 = addTax2(0.2);
+// addVAT2(100); // 120
+
+// /* -------------------------------- Function Methods et Event Listener -------------------------------- */
+
+// /* Je crée une nouvelle propriété pour l'Object lufthansa et je définis la valeur à 300 */
+// lufthansa.planes = 300;
+
+// /* Je crée une nouvelle Method pour l'Object lufthansa, le "this" pointe vers lufthansa */
+// lufthansa.buyPlane = function () {
+// 	this.planes++;
+// 	console.log(this);
+// 	console.log(this.planes);
+// };
+
+// /* La Method fonctionne correctement car le this keyword pointe vers lufthansa */
+// // lufthansa.buyPlane(); // 301
+
+// /* Dans le cas d'un Event Listener, la handler function "lufthansa.buyPlane" sera call au moment du clic du bouton
+// mais cela retourne NaN et retourne l'élément html du bouton. Pourquoi ? Car le this keyword de la handler function
+// (lufthansa.buyPlane) est attaché à "document.querySelector(".buy") donc cela fausse toute la fonction. */
+// // document.querySelector(".buy").addEventListener("click", lufthansa.buyPlane); // cf. Console (au moment du clic)
+
+// /* Pour résoudre ce problème il faut utiliser rediriger le .this keyword vers lufthansa : .call ou .bind ?
+// La réponse est évidemment .bind car nous voulons pas call la fonction tout de suite (ce que ferait .call Method)
+// nous voulons juste rediriger le .this de la handler function qui sera utilisé plus tard (au moment du clic) */
+// document
+// 	.querySelector(".buy")
+// 	.addEventListener("click", lufthansa.buyPlane.bind(lufthansa));
