26b875ac7e9576add4e3f48cfde3dde47730e329
Ekiz Cem <ekizcem.pro@gmail.com>
2022-06-25 20:31:45 +0200

    Implementing Login

--- 11-Arrays--Bankist/script.js
+++ 11-Arrays--Bankist/script.js
@@ -92,8 +92,6 @@ const displayMovements = function (movements) {
 	});
 };
 
-displayMovements(account1.movements);
-
 ////////////////// calcDisplayBalance Function //////////////////
 
 const calcDisplayBalance = function (movements) {
@@ -101,31 +99,27 @@ const calcDisplayBalance = function (movements) {
 	labelBalance.textContent = `${balance} EUR`;
 };
 
-calcDisplayBalance(account1.movements);
-
 ////////////////// calcDisplaySummary Function //////////////////
 
-const calcDisplaySummary = function (movements) {
-	const incomes = movements
+const calcDisplaySummary = function (account) {
+	const incomes = account.movements
 		.filter((mov) => mov > 0)
 		.reduce((acc, mov) => acc + mov, 0);
 	labelSumIn.textContent = `${incomes}€`;
 
-	const out = movements
+	const out = account.movements
 		.filter((mov) => mov < 0)
 		.reduce((acc, mov) => acc + mov, 0);
 	labelSumOut.textContent = `${Math.abs(out)}€`;
 
-	const interest = movements
+	const interest = account.movements
 		.filter((mov) => mov > 0)
-		.map((deposit) => deposit * (1.2 / 100))
+		.map((deposit) => deposit * (account.interestRate / 100))
 		.filter((int) => int >= 1)
 		.reduce((acc, int) => acc + int, 0);
 
 	labelSumInterest.textContent = `${interest}`;
 };
-
-calcDisplaySummary(account1.movements);
 
 ////////////////// createUsernames Function //////////////////
 
@@ -144,6 +138,42 @@ const createUsernames = function (account) {
 createUsernames(accounts);
 
 // console.log(accounts); //> cf. Console
+
+////////////////// Implementing Login //////////////////
+
+// Event Handlers
+let currentAccount;
+
+btnLogin.addEventListener("click", function (e) {
+	e.preventDefault();
+
+	currentAccount = accounts.find(
+		(acc) => acc.username === inputLoginUsername.value
+	);
+
+	console.log(currentAccount);
+
+	if (currentAccount?.pin === Number(inputLoginPin.value)) {
+		// Display UI and Welcome Message
+		labelWelcome.textContent = `Welcome back, ${
+			currentAccount.owner.split(" ")[0]
+		}`;
+		containerApp.style.opacity = 100;
+
+		// Clear input fields
+		inputLoginUsername.value = inputLoginPin.value = "";
+		inputLoginPin.blur();
+
+		// Display Movements
+		displayMovements(currentAccount.movements);
+
+		// Display Balance
+		calcDisplayBalance(currentAccount.movements);
+
+		// Display Summary
+		calcDisplaySummary(currentAccount);
+	}
+});
 
 // ========================================================================== //
 // ========================================================================== //
