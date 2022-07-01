a41552579d2b6240e58a2b5075f34a0e03b7fcf0
Ekiz Cem <ekizcem.pro@gmail.com>
2022-07-01 20:21:39 +0200

    Implementing : Intl Numbers

--- 12-Numbers-Dates-Timers/app.js
+++ 12-Numbers-Dates-Timers/app.js
@@ -88,7 +88,7 @@ const formatMovementDate = function (date, locale) {
 		Math.round(Math.abs((date2 - date1) / (1000 * 60 * 60 * 24)));
 
 	const daysPassed = calcDaysPassed(new Date(), date);
-	console.log(daysPassed);
+	// console.log(daysPassed);
 
 	if (daysPassed === 0) return "Today";
 	if (daysPassed === 1) return "Yesterday";
@@ -100,6 +100,15 @@ const formatMovementDate = function (date, locale) {
 		// return `${day}/${month}/${year}`;
 	}
 	return new Intl.DateTimeFormat(locale).format(date);
+};
+
+///////////////////////////// formatMovementDate Function /////////////////////////////
+
+const formatCur = function (value, locale, currency) {
+	return new Intl.NumberFormat(locale, {
+		style: "currency",
+		currency: currency,
+	}).format(value);
 };
 
 ///////////////////////////// displayMovements Function /////////////////////////////
@@ -122,6 +131,8 @@ const displayMovements = function (acc, sort = false) {
 		const date = new Date(acc.movementsDates[i]);
 		const displayDate = formatMovementDate(date, acc.locale);
 
+		const formattedMov = formatCur(mov, acc.locale, acc.currency);
+
 		// Cibler le contenu HTML à ajouter
 		const html = `
 		<div class="movements__row">
@@ -129,7 +140,7 @@ const displayMovements = function (acc, sort = false) {
 		${i + 1} ${type}
 		</div>
 		<div class="movements__date">${displayDate}</div>
-		<div class="movements__value">${mov.toFixed(2)}€</div>
+		<div class="movements__value">${formattedMov}</div>
 		</div>
 		`;
 
@@ -142,29 +153,30 @@ const displayMovements = function (acc, sort = false) {
 
 const calcDisplayBalance = function (acc) {
 	acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-	labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
+
+	labelBalance.textContent = formatCur(acc.balance, acc.locale, acc.currency);
 };
 
 ///////////////////////////// calcDisplaySummary Function /////////////////////////////
 
-const calcDisplaySummary = function (account) {
-	const incomes = account.movements
+const calcDisplaySummary = function (acc) {
+	const incomes = acc.movements
 		.filter((mov) => mov > 0)
 		.reduce((acc, mov) => acc + mov, 0);
-	labelSumIn.textContent = `${incomes.toFixed(2)}€`;
+	labelSumIn.textContent = formatCur(incomes, acc.locale, acc.currency);
 
-	const out = account.movements
+	const out = acc.movements
 		.filter((mov) => mov < 0)
 		.reduce((acc, mov) => acc + mov, 0);
-	labelSumOut.textContent = `${Math.abs(out).toFixed(2)}€`;
+	labelSumOut.textContent = formatCur(Math.abs(out), acc.locale, acc.currency);
 
-	const interest = account.movements
+	const interest = acc.movements
 		.filter((mov) => mov > 0)
-		.map((deposit) => deposit * (account.interestRate / 100))
+		.map((deposit) => deposit * (acc.interestRate / 100))
 		.filter((int) => int >= 1)
 		.reduce((acc, int) => acc + int, 0);
 
-	labelSumInterest.textContent = `${interest.toFixed(2)}€`;
+	labelSumInterest.textContent = formatCur(interest, acc.locale, acc.currency);
 };
 
 ///////////////////////////// createUsernames Function /////////////////////////////
