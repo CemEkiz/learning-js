c1bc4965e96585193ad20ae1bee8cf208f00a011
Ekiz Cem <ekizcem.pro@gmail.com>
2022-07-02 16:55:54 +0200

    Coding Challenge 4 about Arrays Methods

--- 00-Coding-Challenges/11-arrays.js
+++ 00-Coding-Challenges/11-arrays.js
@@ -113,34 +113,95 @@
 
 /* -------------------------------------------------------------------------------- */
 /* -------------------------------------------------------------------------------- */
-/* ------------------------------ Coding Challenge 2 ------------------------------ */
+/* ------------------------------ Coding Challenge 3 ------------------------------ */
 /* -------------------------------------------------------------------------------- */
 /* -------------------------------------------------------------------------------- */
 
 // __________________ Ma Solution __________________
 
-const dogsAge1 = [5, 2, 4, 1, 15, 8, 3];
-const dogsAge2 = [16, 6, 10, 5, 6, 1, 4];
+// const dogsAge1 = [5, 2, 4, 1, 15, 8, 3];
+// const dogsAge2 = [16, 6, 10, 5, 6, 1, 4];
 
-const calcAverageHumanAge2 = (ages) => {
-	const humanAges = ages
-		.map((age) => (age <= 2 ? 2 * age : 16 + age * 4))
-		.filter((age) => age >= 18)
-		.reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+// const calcAverageHumanAge2 = (ages) => {
+// 	const humanAges = ages
+// 		.map((age) => (age <= 2 ? 2 * age : 16 + age * 4))
+// 		.filter((age) => age >= 18)
+// 		.reduce((acc, age, i, arr) => acc + age / arr.length, 0);
 
-	return humanAges;
-};
+// 	return humanAges;
+// };
 
-const avg1 = calcAverageHumanAge2(dogsAge1);
-// (7) [36, 4, 32, 2, 76, 48, 28]
-// (5) [36, 32, 76, 48, 28]
-const avg2 = calcAverageHumanAge2(dogsAge2);
-// (7) [80, 40, 56, 36, 40, 2, 32]
-// (6) [80, 40, 56, 36, 40, 32]
+// const avg1 = calcAverageHumanAge2(dogsAge1);
+// // (7) [36, 4, 32, 2, 76, 48, 28]
+// // (5) [36, 32, 76, 48, 28]
+// const avg2 = calcAverageHumanAge2(dogsAge2);
+// // (7) [80, 40, 56, 36, 40, 2, 32]
+// // (6) [80, 40, 56, 36, 40, 32]
 
-console.log(avg1); // 44
-console.log(avg2); // 47.333333333333336
+// console.log(avg1); // 44
+// console.log(avg2); // 47.333333333333336
 
 // __________________ Solution de Jonas __________________
 
-/* cf. Ma Solution */
+// /* cf. Ma Solution */
+
+/* -------------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------------- */
+/* ------------------------------ Coding Challenge 4 ------------------------------ */
+/* -------------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------------- */
+
+// // __________________ Solution de Jonas __________________
+
+// const dogs = [
+// 	{ weight: 22, curFood: 250, owners: ["Alice", "Bob"] },
+// 	{ weight: 8, curFood: 200, owners: ["Matilda"] },
+// 	{ weight: 13, curFood: 275, owners: ["Sarah", "John"] },
+// 	{ weight: 32, curFood: 340, owners: ["Michael"] },
+// ];
+
+// // 1.
+// dogs.forEach((dog) => (dog.recFood = Math.trunc(dog.weight ** 0.75 * 28)));
+
+// // 2.
+// const dogSarah = dogs.find((dog) => dog.owners.includes("Sarah"));
+// console.log(dogSarah);
+// console.log(
+// 	`Sarah's dog is eating too ${
+// 		dogSarah.curFood > dogSarah.recFood ? "much" : "little"
+// 	} `
+// );
+
+// // 3.
+// const ownersEatTooMuch = dogs
+// 	.filter((dog) => dog.curFood > dog.recFood)
+// 	.flatMap((dog) => dog.owners);
+// // .flat();
+// console.log(ownersEatTooMuch);
+// const ownersEatTooLittle = dogs
+// 	.filter((dog) => dog.curFood < dog.recFood)
+// 	.flatMap((dog) => dog.owners);
+// console.log(ownersEatTooLittle);
+
+// // 4.
+// // "Matilda and Alice and Bob's dogs eat too much!"
+// //  "Sarah and John and Michael's dogs eat too little!"
+// console.log(`${ownersEatTooMuch.join(" and ")}'s dogs eat too much!`);
+// console.log(`${ownersEatTooLittle.join(" and ")}'s dogs eat too little!`);
+
+// // 5.
+// console.log(dogs.some((dog) => dog.curFood === dog.recFood));
+
+// // 6.
+// // current > (recommended * 0.90) && current < (recommended * 1.10)
+// const checkEatingOkay = (dog) =>
+// 	dog.curFood > dog.recFood * 0.9 && dog.curFood < dog.recFood * 1.1;
+// console.log(dogs.some(checkEatingOkay));
+
+// // 7.
+// console.log(dogs.filter(checkEatingOkay));
+
+// // 8.
+// // sort it by recommended food portion in an ascending order [1,2,3]
+// const dogsSorted = dogs.slice().sort((a, b) => a.recFood - b.recFood);
+// console.log(dogsSorted);
