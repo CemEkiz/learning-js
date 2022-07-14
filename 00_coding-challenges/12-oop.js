4678422bc2655fdc851ffc80e212be32a5029c9f
Ekiz Cem <ekizcem.pro@gmail.com>
2022-07-14 10:23:44 +0200

    Coding Challenge about Inheritance Between Classes (with Constructor Function)

--- 00_coding-challenges/12-oop.js
+++ 00_coding-challenges/12-oop.js
@@ -110,7 +110,7 @@
 
 // 	// Setter
 // 	set speedUS(speed) {
-// 		this.speed /= 1.6;
+// 		this.speed = speed * 1.6;
 // 	}
 // }
 
@@ -130,3 +130,99 @@
 // // 3.
 // ford.speedUS = 50;
 // console.log(ford.speed);
+
+/* -------------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------------- */
+/* ------------------------------ Coding Challenge 3 ------------------------------ */
+/* -------------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------------- */
+
+// /*
+
+// 1. Use a constructor function to implement an Electric Car (called 'EV') as a child
+// "class" of 'Car'. Besides a make and current speed, the 'EV' also has the
+// current battery charge in % ('charge' property)
+
+// 2. Implement a 'chargeBattery' method which takes an argument
+// 'chargeTo' and sets the battery charge to 'chargeTo'
+
+// 3. Implement an 'accelerate' method that will increase the car's speed by 20,
+// and decrease the charge by 1%. Then log a message like this: 'Tesla going at 140
+// km/h, with a charge of 22%'
+
+// 4. Create an electric car object and experiment with calling 'accelerate',
+// 'brake' and 'chargeBattery' (charge to 90%). Notice what happens when
+// you 'accelerate'!
+
+// Hint: Review the definiton of polymorphism 😉
+
+// Test data:
+
+// - Data car 1: 'Tesla' going at 120 km/h, with a charge of 23%
+
+// */
+
+// const Car = function (make, speed) {
+// 	this.make = make;
+// 	this.speed = speed;
+// };
+
+// Car.prototype.accelerate = function () {
+// 	this.speed += 10;
+// 	console.log(`The ${this.make} is going at ${this.speed} km/h`);
+// };
+
+// Car.prototype.brake = function () {
+// 	this.speed -= 5;
+// 	console.log(`The ${this.make} is going at ${this.speed} km/h`);
+// };
+
+// // 1.
+// const EV = function (make, speed, charge) {
+// 	Car.call(this, make, speed);
+// 	this.charge = charge;
+// };
+
+// EV.prototype = Object.create(Car.prototype);
+
+// // 2.
+// EV.prototype.chargeBattery = function (chargeTo) {
+// 	this.charge = chargeTo;
+// };
+
+// // 3.
+// EV.prototype.accelerate = function () {
+// 	this.speed += 20;
+// 	this.charge--;
+// 	console.log(
+// 		`Tesla going at ${this.speed} km/h, with a charge of ${this.charge}`
+// 	);
+// };
+
+// // 4.
+// const tesla = new EV('Tesla', 120, 23);
+
+// console.log(tesla);
+
+// tesla.accelerate();
+// tesla.accelerate();
+// tesla.accelerate();
+// tesla.chargeBattery(90);
+// tesla.accelerate();
+// tesla.accelerate();
+// tesla.brake();
+// tesla.accelerate();
+
+// console.log(tesla);
+
+// // Tests
+// // console.log(tesla.__proto__);
+// // console.log(tesla.__proto__.__proto__);
+// // console.log(tesla.__proto__.__proto__.__proto__);
+
+// // console.log(tesla instanceof EV); // true
+// // console.log(tesla instanceof Car); // true
+
+// // console.dir(EV.prototype.constructor); // Car
+// // EV.prototype.constructor = EV;
+// // console.dir(EV.prototype.constructor); // Student
