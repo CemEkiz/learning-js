b5bbb55da4e74ca2437e496750b6c70f98877b9a
Ekiz Cem <ekizcem.pro@gmail.com>
2022-07-14 14:21:10 +0200

    Final coding challenge about ES6 Classes

--- 00_coding-challenges/12-oop.js
+++ 00_coding-challenges/12-oop.js
@@ -226,3 +226,92 @@
 // // console.dir(EV.prototype.constructor); // Car
 // // EV.prototype.constructor = EV;
 // // console.dir(EV.prototype.constructor); // Student
+
+/* -------------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------------- */
+/* ------------------------------ Coding Challenge 4 ------------------------------ */
+/* -------------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------------- */
+
+// /*
+
+// 1. Re-create Challenge #3, but this time using ES6 classes: create an 'EVCl'
+// child class of the 'CarCl' class
+
+// 2. Make the 'charge' property private
+
+// 3. Implement the ability to chain the 'accelerate' and 'chargeBattery'
+// methods of this class, and also update the 'brake' method in the 'CarCl'
+// class. Then experiment with chaining!
+
+// Test data:
+
+// - Data car 1: 'Rivian' going at 120 km/h, with a charge of 23%
+
+// */
+
+// // 1.
+// class CarCl {
+// 	constructor(make, speed) {
+// 		this.make = make;
+// 		this.speed = speed;
+// 	}
+
+// 	accelerate() {
+// 		this.speed += 10;
+// 		console.log(`The ${this.make} is going at ${this.speed} km/h`);
+// 		return this;
+// 	}
+
+// 	brake() {
+// 		this.speed -= 5;
+// 		console.log(`The ${this.make} is going at ${this.speed} km/h`);
+// 		return this;
+// 	}
+
+// 	// Getter
+// 	get speedUS() {
+// 		return this.speed / 1.6;
+// 	}
+
+// 	// Setter
+// 	set speedUS(speed) {
+// 		this.speed = speed * 1.6;
+// 	}
+// }
+
+// class EVCl extends CarCl {
+// 	#charge;
+
+// 	constructor(make, speed, charge) {
+// 		super(make, speed);
+// 		this.#charge = charge;
+// 	}
+
+// 	chargeBattery(chargeTo) {
+// 		this.#charge = chargeTo;
+// 		console.log(`The battery of the ${this.make} is now at ${this.#charge}%`);
+// 		return this;
+// 	}
+
+// 	accelerate() {
+// 		this.speed += 20;
+// 		this.#charge--;
+// 		console.log(
+// 			`${this.make} going at ${this.speed} km/h, with a charge of ${
+// 				this.#charge
+// 			}`
+// 		);
+// 		return this;
+// 	}
+// }
+
+// const rivian = new EVCl('Rivian', 120, 23);
+// console.log(rivian); // EVCl {make: 'Rivian', speed: 120, charge: 23}
+
+// // console.log(rivian.#charge); // SyntaxeError : Private field "#charge"...
+
+// // With Chaining (cf. Console)
+// rivian.accelerate().accelerate().brake().brake().accelerate().chargeBattery(40);
+
+// console.log(rivian.speedUS); // 106.25
